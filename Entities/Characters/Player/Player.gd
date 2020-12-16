ba60747a95f9d9344e4ce6f6e892dffbb540d159
Kevin Wagner <klwagne1@gmail.com>
2020-12-15 22:44:58 -0500

    Refactoring spaghetti code

--- Entities/Characters/Player/Player.gd
+++ Entities/Characters/Player/Player.gd
@@ -4,24 +4,33 @@ extends RigidBody2D
 var rng = RandomNumberGenerator.new()
 
 #exports
+export var egg_texture : Texture = Texture.new()
+export var chick_texture : Texture = Texture.new()
 export var torque_force = 50
 export var shard_randomize_factor = .1 #should be between 0 and .33
 export var shard_length = 16
 export var alpha_threshhold = .5
 
-func _ready():
+func _ready():	
 	rng.randomize()
 	
-	$CollisionPolygon2D.polygon = find_sprite_outline($Sprite.texture, shard_length)
+	$CollisionPolygon2D.polygon = find_sprite_outline(egg_texture, 10)
 	$ChickPolygon2D.polygon = $CollisionPolygon2D.polygon
 	generate_shell($CollisionPolygon2D.polygon)
 	
 
-func _process(delta):
-	#update labels
-	$VelocityLabel.text = "Velocity: " + str(linear_velocity)
+func _integrate_forces(state):
+	if state.get_contact_count() > 0:
+		if Input.is_action_pressed("ui_left"):
+			apply_torque_impulse(-torque_force)
+		if Input.is_action_pressed("ui_right"):
+			apply_torque_impulse(torque_force)
+	
+	if Input.is_action_just_pressed("ui_select") and state.get_contact_count() > 0:
+		_break_shell(to_local(state.get_contact_collider_position(0)), 500)
 
 
+# trace outline of given texture and return array of points for polygon generation
 func find_sprite_outline(texture, point_resolution) -> PoolVector2Array:
 	var outline_image = texture.get_data()
 	var outline_image_offset = Vector2(-outline_image.get_width() / 2, -outline_image.get_height() / 2)
@@ -62,6 +71,7 @@ func find_sprite_outline(texture, point_resolution) -> PoolVector2Array:
 	return outline_points
 
 
+# generate collision and texture polygons based on generated sprite outline points from above
 func generate_shell(points : PoolVector2Array):
 	for i in range(len(points)):
 		var point_interpolate_count = int(points[i].distance_to(Vector2.ZERO) / shard_length)
@@ -82,7 +92,7 @@ func generate_shell(points : PoolVector2Array):
 		var shard = Polygon2D.new()
 		shard.polygon = shard_points
 		#shard.color = Color(randf(),randf(),randf(),1)
-		var shard_texture = $Sprite.texture
+		var shard_texture = egg_texture
 		var shard_image = shard_texture.get_data()
 		var shard_texture_offset = Vector2(shard_image.get_width() / 2, shard_image.get_height() / 2)
 		shard.texture = shard_texture
@@ -91,22 +101,7 @@ func generate_shell(points : PoolVector2Array):
 		$ShellPolygons.add_child(shard)
 
 
-func _integrate_forces(state):
-	if Input.is_action_pressed("ui_left"):
-		apply_torque_impulse(-torque_force)
-	if Input.is_action_pressed("ui_right"):
-		apply_torque_impulse(torque_force)
-	
-	if Input.is_action_just_pressed("ui_select") and state.get_contact_count() > 0:
-		#mode = MODE_KINEMATIC
-		_break_shell(to_local(state.get_contact_collider_position(0)), 500)
-		#mode = MODE_RIGID
-	
-	if state.get_contact_count() > 0:
-		$ContactLabel.text = "Contact: " + str(state.get_contact_collider_position(0))
-		$ContactSprite.global_position = state.get_contact_collider_position(0)
-	
-
+# apply break to shell and remove shell fragment from player object
 func _break_shell(collider_position : Vector2, damage : float):
 	var polygon_scores = []
 	var polygons = []
@@ -134,40 +129,19 @@ func _break_shell(collider_position : Vector2, damage : float):
 	#remove closest polygons from polygon array and add to array for removal
 	var polygons_to_remove = []
 	while damage > 0 and polygons.size() > 0:
-		print(polygons.size())
 		polygons_to_remove.append(polygons[0])
 		damage -= _get_polygon_area(polygons[0])
 		polygons.remove(0)
 	
 	if polygons_to_remove.size() > 0:
-		#create new rigidbody2d based on polygons in array for removal, as well as a collision and 2D polygon
-		var shard_body = RigidBody2D.new()
-		var shard_collision_polygon = CollisionPolygon2D.new()
-		var shard_polygon = Polygon2D.new()
-		
-		#set polygon points to combined polygon outline
-		shard_collision_polygon.polygon = _get_combined_polygon_outline(polygons_to_remove.duplicate())
-		shard_polygon.polygon = shard_collision_polygon.polygon
-		
-		#update polygon2D's texture
-		var shard_polygon_texture = $Sprite.texture
-		var shard_polygon_image = shard_polygon_texture.get_data()
-		var shard_polygon_texture_offset = Vector2(shard_polygon_image.get_width() / 2, shard_polygon_image.get_height() / 2)
-		shard_polygon.texture = shard_polygon_texture
-		shard_polygon.texture_offset = shard_polygon_texture_offset
-		
-		#cut shard shape out off egg collision shape
-		#$CollisionPolygon2D.polygon = _get_clipped_polygon_outline($CollisionPolygon2D.duplicate(), shard_polygon.duplicate())
-		
-		#set shards collision layer to avoid colliding with egg
-		shard_body.collision_layer = 0b0
-		shard_body.collision_mask = 0b1
-		
-		#add egg shard as a child of the scene
-		shard_body.add_child(shard_collision_polygon)
-		shard_body.add_child(shard_polygon)
-		#shard_body.global_position = global_position
-		add_child(shard_body)
+	
+		# create a new shard instance and populate shard data
+		var shard_instance = preload("res://Entities/Characters/Player/Shard/Shard.tscn").instance()
+		shard_instance.polygon_points = _get_combined_polygon_outline(polygons_to_remove.duplicate())
+		shard_instance.texture_polygon_texture = egg_texture
+		shard_instance.global_position = global_position + shard_instance.position
+		shard_instance.global_rotation = global_rotation
+		get_tree().root.add_child(shard_instance)
 		
 		
 		#clear polygons to remove array
@@ -175,14 +149,7 @@ func _break_shell(collider_position : Vector2, damage : float):
 			polygon.queue_free()
 
 
-func _get_clipped_polygon_outline(polygonA, polygonB) -> PoolVector2Array:
-	var transA = Transform2D(0.0, polygonA.global_position)
-	var transB = Transform2D(0.0, polygonB.global_position)
-	polygonB.polygon = Geometry.offset_polygon_2d(polygonB.polygon, 0.2)[0]
-	polygonA.polygon = transA.xform_inv(Geometry.clip_polygons_2d(transA.xform(polygonA.polygon), transB.xform(polygonB.polygon))[0])
-	return polygonA.polygon
-
-
+# takes in a list of polygons and returns the points of a single combined polygon
 func _get_combined_polygon_outline(polygons) -> PoolVector2Array:
 	while polygons.size() > 1:
 		
@@ -208,6 +175,7 @@ func _get_combined_polygon_outline(polygons) -> PoolVector2Array:
 	return polygons[0].polygon
 
 
+# returns the area of a given triangle
 func _get_polygon_area(polygon : Polygon2D) -> float:
 	var point_A = polygon.polygon[0]
 	var point_B = polygon.polygon[1]
---
+++ Entities/Characters/Player/Shard/Shard.gd
@@ -0,0 +1,27 @@
+extends RigidBody2D
+
+#exports
+export var polygon_points : PoolVector2Array = PoolVector2Array() setget _set_polygon_points
+export var texture_polygon_texture : Texture = Texture.new() setget _set_texture_polygon_texture
+
+
+#func _ready():
+#	_set_collision_polygon_points(collision_polygon_points)
+#	_set_texture_polygon_points(texture_polygon_points)
+#	_set_texture_polygon_texture()
+
+
+func _set_polygon_points(new_val):
+	polygon_points = new_val
+	$CollisionPolygon2D.polygon = polygon_points
+	$TexturePolygon2D.polygon = polygon_points
+
+
+func _set_texture_polygon_texture(new_val):
+	texture_polygon_texture = new_val
+	$TexturePolygon2D.texture = texture_polygon_texture
+	
+	# set offset offset to center on shard
+	var texture_polygon_image = texture_polygon_texture.get_data()
+	var texture_polygon_texture_offset = Vector2(texture_polygon_image.get_width() / 2, texture_polygon_image.get_height() / 2)
+	$TexturePolygon2D.texture_offset = texture_polygon_texture_offset
